811e2c8fc900dae42f05cdde336897d14289371c
bfelger <bfelger@gmail.com>
2023-05-11 16:02:49 -0400

    Pt4-CMake GCC/Clang on UBuntu

--- src/strings.h
+++ src/strings.h
@@ -8,6 +8,8 @@
 #ifndef ROM__STRINGS_H
 #define ROM__STRINGS_H
 
+#include <ctype.h>
+
 // All strings in ROM are 7-bit POSIX Locale, with the sign bit reserved for 
 // special telnet characters. These macros keep 'ctype.h' functions copacetic
 // with char* strings under -Wall.
@@ -25,5 +27,9 @@
     #define ISSPACE(c) isspace(c)
     #define ISUPPER(c) isupper(c)
 #endif
+
+// isascii() is marked OB by POSIX, but we still need to test for telnet-
+// friendly characters.
+#define ISASCII(c) ((unsigned char)(c) & ~0x7F)
 
 #endif // !ROM__STRINGS_H
