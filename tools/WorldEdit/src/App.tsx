2e003607eb793e14eeb8bb57e9b2d58f716fa782
bfelger <bfelger@gmail.com>
2026-01-13 13:51:04 -0500

    Initial scaffolding

---
+++ tools/WorldEdit/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main className="app">
+      <div className="app__badge">Mud98</div>
+      <h1 className="app__title">WorldEdit</h1>
+      <p className="app__subtitle">Desktop editor for Mud98 JSON areas.</p>
+    </main>
+  );
+}
